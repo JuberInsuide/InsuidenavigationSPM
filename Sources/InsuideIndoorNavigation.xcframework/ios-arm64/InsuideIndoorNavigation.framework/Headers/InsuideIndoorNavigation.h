6b46959cb049404c5e13add4f2dbda736b28737c
JuberInsuide <juber@insuide.com>
2024-09-01 23:40:42 +0530

    first commit

---
+++ Sources/InsuideIndoorNavigation.xcframework/ios-arm64/InsuideIndoorNavigation.framework/Headers/InsuideIndoorNavigation.h
@@ -0,0 +1,18 @@
+//
+//  InsuideIndoorNavigation.h
+//  InsuideIndoorNavigation
+//
+//  Created by juber99 on 28/08/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for InsuideIndoorNavigation.
+FOUNDATION_EXPORT double InsuideIndoorNavigationVersionNumber;
+
+//! Project version string for InsuideIndoorNavigation.
+FOUNDATION_EXPORT const unsigned char InsuideIndoorNavigationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <InsuideIndoorNavigation/PublicHeader.h>
+
+
